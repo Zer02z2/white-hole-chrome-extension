fe211a8c1010060f1fae94c4ce327cdc28ba46a6
Zer02z2 <zongzechan@gmail.com>
2024-11-04 00:43:06 -0500

    parent null

--- whiteHole.js
+++ whiteHole.js
@@ -1,47 +1,67 @@
-const queryTargets = ["div", "span", "img", "input"]
-const elementsNodes = document.body.querySelectorAll("*")
-let elementsArray = Array.from(elementsNodes)
-
 const centerX = window.innerWidth / 2
 const centerY = window.innerHeight / 2
 
-const findEndChildren = (elements) => {
+const elementData = (element) => {
+  const rect = element.getBoundingClientRect()
+  const x = rect.left + rect.width / 2 + window.scrollX
+  const y = rect.top + rect.height / 2 + window.scrollY
+  const moveX = centerX - x
+  const moveY = centerY - y
+  const increment = 0.01
+  const progress = 0
+  return {
+    target: element,
+    moveX: moveX,
+    moveY: moveY,
+    increment: increment,
+    progress: progress,
+  }
+}
+
+const findEndChildren = () => {
+  console.log("hi")
+  const elementsNodes = document.body.querySelectorAll("*")
+  const elements = Array.from(elementsNodes)
   let result = []
-  for (let i = elements.length - 1; i > 0; i--) {
+  for (let i = elements.length - 1; i >= 0; i--) {
     const element = elements[i]
     const children = element.querySelectorAll(":scope > *")
     if (Array.from(children).length === 0) {
-      const rect = element.getBoundingClientRect()
-      const x = rect.left + rect.width / 2 + window.scrollX
-      const y = rect.top + rect.height / 2 + window.scrollY
-      const moveX = centerX - x
-      const moveY = centerY - y
-      const increment = 0.001
-      const progress = 0
-      result.push({
-        target: element,
-        moveX: moveX,
-        moveY: moveY,
-        increment: increment,
-        progress: progress,
-      })
-      elements.splice(i, 1)
+      result.push(elementData(element))
     }
   }
   return result
 }
 
-let endChildren = findEndChildren(elementsArray)
+let endChildren = findEndChildren()
+let watchList = []
 
 const animate = () => {
-  endChildren.forEach((element) => {
+  console.log(endChildren)
+  for (let i = endChildren.length - 1; i >= 0; i--) {
+    const element = endChildren[i]
     const { target, moveX, moveY, increment, progress } = element
     element.progress = progress + increment
     target.style.transform = `translateX(${moveX * progress}px) translateY(${
       moveY * progress
     }px)`
-  })
-  for (let i = endChildren.length - 1; i > 0; i--) {}
+
+    if (element.progress >= 1) {
+      endChildren.splice(i, 1)
+      const parent = target.parentNode
+      parent.removeChild(target)
+      if (watchList.find((item) => item === parent)) break
+      watchList.push(parent)
+    }
+  }
+
+  for (let i = watchList.length - 1; i >= 0; i--) {
+    const element = watchList[i]
+    if (element.querySelectorAll(":scope > *").length === 0) {
+      watchList.splice(i, 1)
+      endChildren.push(elementData(element))
+    }
+  }
   window.requestAnimationFrame(animate)
 }
 animate()
