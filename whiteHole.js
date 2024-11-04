b28a7cd63e915a2e3e5eda0308487d03b0e86447
Zer02z2 <zongzechan@gmail.com>
2024-11-04 13:13:50 -0500

    size, opacity, and parents

--- whiteHole.js
+++ whiteHole.js
@@ -1,25 +1,31 @@
 const centerX = window.innerWidth / 2
 const centerY = window.innerHeight / 2
 
+const random = (low, high) => {
+  return low + Math.random() * (high - low)
+}
+
 const elementData = (element) => {
   const rect = element.getBoundingClientRect()
   const x = rect.left + rect.width / 2 + window.scrollX
   const y = rect.top + rect.height / 2 + window.scrollY
   const moveX = centerX - x
   const moveY = centerY - y
-  const increment = 0.01
-  const progress = 0
+  const increment = random(0.002, 0.01)
+  const delay = random(0, 300)
   return {
     target: element,
     moveX: moveX,
     moveY: moveY,
     increment: increment,
-    progress: progress,
+    progress: 0,
+    delay: delay,
+    opacity: 1,
+    scale: 1,
   }
 }
 
 const findEndChildren = () => {
-  console.log("hi")
   const elementsNodes = document.body.querySelectorAll("*")
   const elements = Array.from(elementsNodes)
   let result = []
@@ -29,6 +35,7 @@ const findEndChildren = () => {
     if (Array.from(children).length === 0) {
       result.push(elementData(element))
     }
+    //if (result.length === 10000) break
   }
   return result
 }
@@ -37,23 +44,33 @@ let endChildren = findEndChildren()
 let watchList = []
 
 const animate = () => {
-  console.log(endChildren)
   for (let i = endChildren.length - 1; i >= 0; i--) {
     const element = endChildren[i]
-    const { target, moveX, moveY, increment, progress } = element
+    const { target, moveX, moveY, increment, progress, opacity, delay, scale } =
+      element
+    element.delay = delay - 1
+    if (delay > 0) continue
+
     element.progress = progress + increment
+    element.opacity = opacity - increment
+    element.scale = scale - increment
     target.style.transform = `translateX(${moveX * progress}px) translateY(${
       moveY * progress
     }px)`
+    target.style.opacity = opacity
+    target.style.scale = scale
 
     if (element.progress >= 1) {
       endChildren.splice(i, 1)
       const parent = target.parentNode
+      if (!parent) break
       parent.removeChild(target)
       if (watchList.find((item) => item === parent)) break
       watchList.push(parent)
     }
   }
+
+  //if (endChildren.length === 0) endChildren = findEndChildren()
 
   for (let i = watchList.length - 1; i >= 0; i--) {
     const element = watchList[i]
